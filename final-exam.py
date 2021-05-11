4769131c7391403d10f5b2a8c9a54cd41173671c
jmsainsbury-matc <jmsainsbury@madisoncollege.edu>
2021-05-11 18:06:45 -0500

    Ready for grading

--- final-exam.py
+++ final-exam.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, requests, bs4, json
+import argparse, requests, bs4, json, socket
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-f', '--function-number', dest='FUNCTION_NUMBER', default=1, type=int, help='Select a function')
@@ -9,28 +9,28 @@ SELECT_FUNCT = str(parser.parse_args().FUNCTION_NUMBER)
 IP = parser.parse_args().IP_ADD
 URL = "http://"+IP+"/q"+SELECT_FUNCT
 if int(SELECT_FUNCT) < 5:
-	print(SELECT_FUNCT)
 	res = requests.get(URL)
 	res.raise_for_status()
 	soup = bs4.BeautifulSoup(res.text, 'html.parser')
+if int(SELECT_FUNCT) == 5:
+	RHOST = IP
+	RPORTS = range(5000,6000)
+	SND_DATA = 'secret'
 print("Name: Josh Sainsbury")
+print(URL)
 def get_response():
-	print("Question 1 selected")
-	print(URL)
+	#print("Question 1 selected")
 	print(f"ANSWER: {soup}")
 def parse_string():
-	print("Question 2 selected")
-	print(URL)
+	#print("Question 2 selected")
 	h3 = str(soup.select('h3'))
 	print(f"ANWSER: {h3[7:-6:3]}")
 def parse_header():
-	print("Question 3 selected")
-	print(URL)
+	#print("Question 3 selected")
 	headers = res.headers
 	print(f"ANWSER: {headers['MATC-HEADER']}")
 def parse_json():
-	print("Question 4 selected")
-	print(URL)
+	#print("Question 4 selected")
 	json_raw = res
 	json_dict = json.loads(json_raw.text)
 	keynum = 0
@@ -40,8 +40,19 @@ def parse_json():
 			if current_dict['title'] == '1984':
 				print(f"ANSWER: {current_dict['author']}")
 		keynum = keynum + 1
-def q5():
-	print("Question 5 selected")	
+def socket_client():
+	#print("Question 5 selected")
+	for RPORT in RPORTS:
+		C_SOCK = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		C_SOCK.settimeout(2)
+		try:
+			C_SOCK.connect((RHOST,RPORT))
+			C_SOCK.send(bytearray(SND_DATA,'utf8'))
+			RCV_DATA = C_SOCK.recv(1024)
+			print(f"ANWSER: {RCV_DATA.decode('utf-8')}")
+			C_SOCK.close()
+		except socket.error as e:
+			C_SOCK.close()
 if SELECT_FUNCT == str(1):
 	get_response()
 elif SELECT_FUNCT == str(2):
@@ -51,6 +62,6 @@ elif SELECT_FUNCT == str(3):
 elif SELECT_FUNCT == str(4):
 	parse_json()
 elif SELECT_FUNCT == str(5):
-	q5()
+	socket_client()
 else:
 	print("Enter 1-5")
